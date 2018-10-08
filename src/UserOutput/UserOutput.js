a9f3064d1967c14e22eb73ed55842a6b5b321db2
GRUPOAMIL\pribeiro05 <pribeiro05@prestadores.amil.com.br>
2018-10-08 17:54:33 -0300

    Exercicio modulo 3

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+	return (
+		<div>
+			<p>{props.name} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus nisi lorem, nec elementum est fringilla ac. Vestibulum consectetur velit quis dolor tempus euismod. Maecenas quis tempus purus. Duis ullamcorper turpis sed lectus lacinia, nec dapibus justo pellentesque. Duis et dui justo. Etiam viverra tellus eros, porttitor sollicitudin ligula varius vel. Praesent ante arcu, vestibulum quis ante ut, finibus vestibulum neque. Morbi at mi id sem malesuada facilisis non et leo. Sed venenatis, ex eu congue ultrices, est sapien tempus leo, id tempus enim magna fringilla diam. Quisque non mollis nisl. Nam interdum, orci in euismod sagittis, nisl lectus suscipit lorem, sed sagittis erat mi quis mauris. Fusce suscipit ante dui, vel porta lectus sodales et.</p>
+			<p>In hac habitasse platea dictumst. Sed mattis tristique orci ut feugiat. Quisque interdum tempus orci a malesuada. Aliquam sapien nibh, scelerisque sit amet rhoncus ut, commodo id libero. Duis consectetur imperdiet sapien vel fringilla. Maecenas scelerisque lorem at magna viverra, a vestibulum mi accumsan. In placerat, enim id facilisis placerat, elit eros imperdiet mauris, et efficitur arcu justo ut dui. Aliquam quis eleifend purus. In auctor lacus eget augue fermentum suscipit. Nunc quis iaculis sapien, eget feugiat lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		</div>
+	);
+}
+
+export default userOutput;
